a6c9a433b3c73ebfc2abb07fdd47952110e7cec2
Bradley Cornwell <bradley@isfluent.com>
2022-02-11 09:15:38 +0000

    Trying event sign up

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,16 @@
 module.exports = {
-    siteMetadata: {
-        siteUrl: `https://www.yourdomain.tld`,
-    },
-    plugins: [
-
-    ]
-}
+	siteMetadata: {
+		siteUrl: `https://www.yourdomain.tld`,
+	},
+	plugins: [
+		{
+			// resolve: 'churchsuite-events-gatsby-plugin',
+			resolve: require.resolve(`../churchsuite-events-gatsby-plugin`),
+			options: {
+				domain: 'thec3.churchsuite.co.uk',
+				date_start: '2019-10-20',
+				date_end: '2019-12-20',
+			},
+		},
+	],
+};
